82a8b136865e0575fc6b816e26195fc88125bae8
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-06-11 10:38:55 +0300

    Refactored some of the code in vIewHitoryFragment

--- app/src/main/java/com/ruth/spellinggame/ui/ViewHistoryFragment.java
+++ app/src/main/java/com/ruth/spellinggame/ui/ViewHistoryFragment.java
@@ -3,63 +3,32 @@ package com.ruth.spellinggame.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
 import com.ruth.spellinggame.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ViewHistoryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ViewHistoryFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import butterknife.BindView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class ViewHistoryFragment extends Fragment {
+    @BindView(R.id.viewHistoryRecyclerView)
+    RecyclerView viewHistoryRecyclerView;
+    private DatabaseReference ref;
+
 
     public ViewHistoryFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ViewHistoryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ViewHistoryFragment newInstance(String param1, String param2) {
-        ViewHistoryFragment fragment = new ViewHistoryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_history, container, false);
+        View view =inflater.inflate(R.layout.fragment_view_history, container, false);
+        return view;
     }
 }
