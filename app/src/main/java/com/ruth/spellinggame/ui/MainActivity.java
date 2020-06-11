cc29edc5392f26ecb147ccd75aaacd5ff79b4b69
rmogusu <rmogusu123@gmail>
2020-06-11 11:31:44 +0200

    Add an intent to navigate to start game fragment.

--- app/src/main/java/com/ruth/spellinggame/ui/MainActivity.java
+++ app/src/main/java/com/ruth/spellinggame/ui/MainActivity.java
@@ -2,9 +2,11 @@ package com.ruth.spellinggame.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.ruth.spellinggame.R;
 
@@ -18,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         mStartGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //do something
+                Intent intent = new Intent(MainActivity.this, GameFragment.class);
+                startActivity(intent);
             }
         });
     }
