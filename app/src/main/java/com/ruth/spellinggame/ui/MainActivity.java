820c7c6513f28de910a902a0c4cd502466395566
rmogusu <rmogusu123@gmail>
2020-06-11 15:19:34 +0200

    Add butterknife.

--- app/src/main/java/com/ruth/spellinggame/ui/MainActivity.java
+++ app/src/main/java/com/ruth/spellinggame/ui/MainActivity.java
@@ -6,12 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ruth.spellinggame.R;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
-    private Button mStartGameButton;
+
+    @BindView(R.id.button ) Button mStartGameButton;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
