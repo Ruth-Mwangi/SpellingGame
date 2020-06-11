128096ae4d4ecf13826086ad158e3a04bbaff5b9
rmogusu <rmogusu123@gmail>
2020-06-11 19:47:17 +0200

    Inflate new menu in an on create option menu.

--- app/src/main/java/com/ruth/spellinggame/ui/MainActivity.java
+++ app/src/main/java/com/ruth/spellinggame/ui/MainActivity.java
@@ -3,6 +3,8 @@ package com.ruth.spellinggame.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.ruth.spellinggame.R;
 
@@ -12,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
